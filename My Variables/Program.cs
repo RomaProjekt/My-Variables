bac0b1edaf7edcabbbc5c75e58a35babef86fcf9
Poplo_000 <Poplo_000@ROMA>
2019-02-06 03:01:56 +0200

    Add project files.

---
+++ My Variables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Myvalue =7;
+            Console.Write("(54)", Myvalue);
+            Console.ReadKey();
+        }
+    }
+}
